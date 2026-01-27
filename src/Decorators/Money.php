77908f4f61a7f756f72df1b1cdd246c4804fa0ae
Vladimir <wowkaster@gmail.com>
2026-01-27 16:09:42 +0000

    Refactor Money decorator to use NumberFormatter

--- src/Decorators/Money.php
+++ src/Decorators/Money.php
@@ -4,13 +4,23 @@ namespace Boduch\Grid\Decorators;
 
 use Boduch\Grid\Cell;
 
+
 class Money
 {
+    private \NumberFormatter $formatter;
+
+    public function __construct(private string $currency = 'USD', private string $locale = 'en_US')
+    {
+        $this->formatter = new \NumberFormatter($locale, \NumberFormatter::CURRENCY);
+    }
+
     /**
      * @param Cell $cell
      */
     public function decorate(Cell $cell)
     {
-        $cell->setValue(money_format('%.2n', (float) $cell->getValue()));
+        $cell->setValue(
+            $this->formatter->formatCurrency((float) $cell->getValue(), $this->currency)
+        );
     }
 }
